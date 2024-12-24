92e5537d891deada016bddefd54e28e46f30bffe
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-12-23 22:30:00 -0300

    init cardapio page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,12 +2,15 @@ import { Route, Routes as RouterDomRoutes } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Login from "./pages/authentication/login/Login";
+import Cardapio from "./pages/client/Cardapio";
 
 export default function Routes() {
   return (
     <RouterDomRoutes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+
+      <Route path="cardapios/:slug" element={<Cardapio />} />
     </RouterDomRoutes>
   );
 }
---
+++ frontend/src/pages/client/Cardapio.tsx
@@ -0,0 +1,3 @@
+export default function Cardapio() {
+  return <h1>tteste</h1>;
+}
