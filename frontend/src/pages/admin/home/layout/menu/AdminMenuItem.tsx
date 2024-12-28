de4f9e1cce892e544aa169c436b053aa5e85a6ff
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2024-12-28 13:21:48 -0300

    init admin menu

--- frontend/src/pages/admin/home/layout/menu/AdminMenuItem.tsx
+++ frontend/src/pages/admin/home/layout/menu/AdminMenuItem.tsx
@@ -1,15 +1,16 @@
 interface AdminMenuItemProps {
   name: string;
   icon: React.ReactNode;
+  isExpanded: boolean;
 }
 
 export default function AdminMenuItem(props: AdminMenuItemProps) {
-  const { name, icon } = props;
-  return (
-    <div className="flex items-center gap-2 w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">
-      <svg className="w-7 h-7">{icon}</svg>
+  const { name, icon, isExpanded } = props;
 
-      <span className="text-sm font-medium">{name}</span>
+  return (
+    <div className="flex items-center gap-1 w-full h-14 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300 overflow-hidden">
+      <span className="w-6 min-w-6 h-6">{icon}</span>
+      {isExpanded && <span className="text-sm font-medium">{name}</span>}
     </div>
   );
 }
---
+++ frontend/src/pages/admin/home/layout/menu/AdminMenuItems.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+import AdminMenuItem from "./AdminMenuItem";
+import HomeIcon from "../../../../../icons/HomeIcon";
+import MetricsIcon from "../../../../../icons/MetricsIcon";
+import CardapioIcon from "../../../../../icons/CardapioIcon";
+
+interface AdminMenuItemsProps {
+  isMenuExpanded: boolean;
+}
+
+export default function AdminMenuItems(props: AdminMenuItemsProps) {
+  const { isMenuExpanded } = props;
+
+  return (
+    <div className="flex flex-col w-full mt-3 border-t-2 border-gray-700 overflow-hidden">
+      <Link to={"/"}>
+        <AdminMenuItem
+          name="Dashboard"
+          icon={<HomeIcon />}
+          isExpanded={isMenuExpanded}
+        />
+      </Link>
+      <Link to={"/"}>
+        <AdminMenuItem
+          name="Métricas"
+          icon={<MetricsIcon />}
+          isExpanded={isMenuExpanded}
+        />
+      </Link>
+      <Link to={"/"}>
+        <AdminMenuItem
+          name="Cardap.io"
+          icon={<CardapioIcon />}
+          isExpanded={isMenuExpanded}
+        />
+      </Link>
+    </div>
+  );
+}
