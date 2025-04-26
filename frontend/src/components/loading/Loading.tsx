68d682be0b3503d34688c726a2ee2fcf7541fe25
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-04-25 21:57:41 -0300

    frontend: adicionando loading

---
+++ frontend/src/components/loading/Loading.tsx
@@ -0,0 +1,7 @@
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+export default function Loading() {
+  return (
+    <Player className="w-full md:w-1/3" autoplay loop speed={0.6} src="/images/bodymovies/loading.json" />
+  );
+}
