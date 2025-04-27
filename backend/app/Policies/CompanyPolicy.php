0b3e3c5015bec4957aac8bc91224250f7948f39f
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-04-27 16:13:32 -0300

    backend: adicionando policy para company

---
+++ backend/app/Policies/CompanyPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Company;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CompanyPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can create the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return bool
+     */
+    public function create(User $user): bool
+    {
+        return is_null($user->company);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Company  $company
+     * @return bool
+     */
+    public function update(User $user, Company $company): bool
+    {
+        return $user->id == $company->owner_id;
+    }
+}
