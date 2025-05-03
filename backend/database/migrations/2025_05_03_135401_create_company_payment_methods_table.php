df38cebb76ff193b05100a085352d65b6ac59538
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2025-05-03 19:53:36 -0300

    backend: adicionando delete cascade no company payment method

--- backend/database/migrations/2025_05_03_135401_create_company_payment_methods_table.php
+++ backend/database/migrations/2025_05_03_135401_create_company_payment_methods_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->integer('company_id')->unsigned();
-            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
 
             $table->enum('payment_method', [
                 'PIX',
